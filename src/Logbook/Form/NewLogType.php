eca45cc38779eb3389bf11ff0dbe4d827de77ad2
tentacode <contact@gabrielpillet.com>
2019-01-22 14:55:28 +0100

    Fixing non required image in log form

--- src/Logbook/Form/NewLogType.php
+++ src/Logbook/Form/NewLogType.php
@@ -36,7 +36,8 @@ class NewLogType extends AbstractType
             ])
             ->add('mediaFile', FileType::class, [
                 'label' => 'Image',
-                'help' => "N'hésitez pas à ajouter une image de votre travail en cours (taille maximum : 2mo)."
+                'help' => "N'hésitez pas à ajouter une image de votre travail en cours (taille maximum : 2mo).",
+                'required' => false,
             ])
             ->add('submit', SubmitType::class, [
                 'label' => 'Ajouter une entrée',
